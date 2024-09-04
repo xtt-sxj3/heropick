1b8285610ba510969ba3f77a7eb2d03f8f462b71
sxj <xtt_sxj3@163.com>
2024-09-04 14:00:34 +0800

    3-3:列表显示

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,29 @@
 const { createApp } = Vue;
 const app = createApp({
   data() {
-    return {};
+    return {
+      heroList: [],
+    };
+  },
+  methods: {},
+  computed: {
+    heroDetail() {
+      return value => {
+        return `https://pvp.qq.com/web201605/herodetail/${value}.shtml`;
+      };
+    },
+    heroImage() {
+      return value => {
+        return `https://game.gtimg.cn/images/yxzj/img201606/heroimg/${value}/${value}.jpg`;
+      };
+    },
+  },
+
+  mounted() {
+    // console.log('当前组件完成挂载初始化');
+    axios.get('http://project.x-zd.net:3001/apis/herolist').then(res => {
+      this.heroList = res.data.data.reverse();
+    });
   },
 });
-
 app.mount('#app');
